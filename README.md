aab5b22fbc04c7be573733ce6848cd7da6dc7a06
bl <bl@bl.local>
2018-08-14 15:14:17 +0800

    a

---
+++ README.md
@@ -0,0 +1,33 @@
+# remote-jenkins-job
+Trigger a Remote Jenkins Job with parameters and get console output in real time, as well as result (pass/fail)
+
+## Background
+This script is intended to replace the Jenkins
+[Parameterized Remote Trigger Plugin](https://wiki.jenkins.io/display/JENKINS/Parameterized+Remote+Trigger+Plugin).
+
+At the time of writing this, the plugin appears unmaintained and the last commit was sometime in August, 2015.  Recent Jenkins updates have made this plugin extremely unstable, resulting in frequent `NullPointerError` failures.
+
+## Requirements
+jq, curl, Jenkins
+
+## Usage
+
+Extremely simple, use as follows:
+
+`remote-job.sh -u https://jenkins-url.com -j JOB_NAME -p "PARAM1=999" -p "PARAM2=123" -t BUILD_TOKEN -i -a john:johnpass`
+
+Where the following parameters are set:
+
+* `-u`: url of jenkins host
+* `-a`: the jenkins user authentication information, e.g. -a user:password or -a user:api_token
+* `-j`: JOB_NAME on jenkins host (eg master-build).
+* `-p`: parameter(s) to pass in. Send multiple parameters by passing in multiple `-p` flags.
+* `-t`: BUILD_TOKEN on remote machine to run job
+* `-i`: ignore certificate validation (useful if you see curl SSL errors while polling)
+
+You can optionally set the polling interval (`POLL_INTERVAL`, default 5) and build timeout (`BUILD_TIMEOUT_SECONDS`, default 3600) as environment variables.
+
+The script will poll the job until completion, and output the console of the running job in near-real time as it is updated.  It does this by setting a cursor on the last line received and only outputting new lines (using a simple tail -n command).
+
+## Notes
+Currently the script outputs all parameters (including tokens) for logging purposes.  You may want to implement a mask on any sensitive fields like tokens through Jenkins password fields.
---
+++ remote-job.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+###
+# Trigger a Remote Jenkins Job with parameters and get console output as well as result
+# Usage:
+# remote-job.sh -u https://jenkins-url.com -j JOB_NAME -p "PARAM1=999" -p "PARAM2=123" -t BUILD_TOKEN
+# -u: url of jenkins host
+# -j: JOB_NAME on jenkins host
+# -p: parameter to pass in. Send multiple parameters by passing in multiple -p flags
+# -t: BUILD_TOKEN on remote machine to run job
+# -i: Tell curl to ignore cert validation
+# -a: Add authentication user
+###
+
+# Number of seconds before timing out
+[ -z "$BUILD_TIMEOUT_SECONDS" ] && BUILD_TIMEOUT_SECONDS=3600
+# Number of seconds between polling attempts
+[ -z "$POLL_INTERVAL" ] && POLL_INTERVAL=10
+while getopts a:j:p:t:u:i opt; do
+  case $opt in
+    p) parameters+=("$OPTARG");;
+    t) parameters+=("token=$OPTARG");;
+    a) USER_AUTH=$OPTARG;;
+    j) JOB_NAME=$OPTARG;;
+    u) JENKINS_URL=$OPTARG;;
+    i) CURL_OPTS="-k" # tell curl to ignore cert validation
+    #...
+  esac
+done
+shift $((OPTIND -1))
+
+if [ ! -z "$USER_AUTH" ]
+then
+  CURL_OPTS="$CURL_OPTS -u $USER_AUTH"
+fi
+
+[ -z "$JENKINS_URL" ] && { echo "JENKINS_URL (-u) not set"; exit 1; }
+echo "JENKINS_URL: $JENKINS_URL"
+[ -z "$JOB_NAME" ] && { echo "JOB_NAME (-j) not set"; exit 1; }
+echo "JOB_NAME: $JOB_NAME"
+
+echo "The whole list of values is '${parameters[@]}'"
+for parameter in "${parameters[@]}"; do
+  # If PARAMS exists, add an ampersand
+  [ -n "$PARAMS" ] && PARAMS=$PARAMS\&$parameter
+  # If no PARAMS exist, don't add an ampersand
+  [ -z "$PARAMS" ] && PARAMS=$parameter
+done
+[ -z "$PARAMS" ] && { echo "No parameters were set!"; exit 1; }
+echo "PARAMS: $PARAMS"
+
+# Queue up the job
+# nb You must use the buildWithParameters build invocation as this
+# is the only mechanism of receiving the "Queued" job id (via HTTP Location header)
+
+REMOTE_JOB_URL="$JENKINS_URL/job/$JOB_NAME/buildWithParameters?$PARAMS"
+echo "Calling REMOTE_JOB_URL: $REMOTE_JOB_URL"
+
+QUEUED_URL=$(curl -sSL $CURL_OPTS -D - $REMOTE_JOB_URL |\
+perl -n -e '/^Location: (.*)$/ && print "$1\n"')
+[ -z "$QUEUED_URL" ] && { echo "No QUEUED_URL was found.  Did you remember to set a token (-t)?"; exit 1; }
+
+# Remove extra \r at end, add /api/json path
+QUEUED_URL=${QUEUED_URL%$'\r'}api/json
+
+# Fetch the executable.url from the QUEUED url
+JOB_URL=`curl -sSL $CURL_OPTS $QUEUED_URL | jq -r '.executable.url'`
+[ "$JOB_URL" = "null" ] && unset JOB_URL
+# Check for status of queued job, whether it is running yet
+COUNTER=0
+while [ -z "$JOB_URL" ]; do
+  echo "The QUEUED counter is $COUNTER"
+  let COUNTER=COUNTER+$POLL_INTERVAL
+  sleep $POLL_INTERVAL
+  if [ "$COUNTER" -gt $BUILD_TIMEOUT_SECONDS ];
+  then
+    break  # Skip entire rest of loop.
+  fi
+  JOB_URL=`curl -sSL $CURL_OPTS $QUEUED_URL | jq -r '.executable.url'`
+  [ "$JOB_URL" = "null" ] && unset JOB_URL
+done
+echo "JOB_URL: $JOB_URL"
+
+# Job is running
+IS_BUILDING="true"
+COUNTER=0
+OUTPUT_LINE_CURSOR=0
+
+# Use until IS_BUILDING = false (instead of while IS_BUILDING = true)
+# to avoid false positives if curl command (IS_BUILDING) fails
+# while polling for status
+until [ "$IS_BUILDING" = "false" ]; do
+  let COUNTER=COUNTER+$POLL_INTERVAL
+  sleep $POLL_INTERVAL
+  if [ "$COUNTER" -gt $BUILD_TIMEOUT_SECONDS ];
+  then
+    echo "TIME-OUT: Exceeded $BUILD_TIMEOUT_SECONDS seconds"
+    break  # Skip entire rest of loop.
+  fi
+  IS_BUILDING=`curl -sSL $CURL_OPTS $JOB_URL/api/json | jq -r '.building'`
+  # Grab total lines in console output
+  NEW_LINE_CURSOR=`curl -sSL $CURL_OPTS $JOB_URL/consoleText | wc -l`
+  # subtract line count from cursor
+  LINE_COUNT=`expr $NEW_LINE_CURSOR - $OUTPUT_LINE_CURSOR`
+  if [ "$LINE_COUNT" -gt 0 ];
+  then
+    curl -sSL $CURL_OPTS $JOB_URL/consoleText | tail -$LINE_COUNT
+  fi
+  OUTPUT_LINE_CURSOR=$NEW_LINE_CURSOR
+done
+
+RESULT=`curl -sSL $CURL_OPTS $JOB_URL/api/json | jq -r '.result'`
+if [ "$RESULT" = 'SUCCESS' ]
+then
+  echo "BUILD RESULT: $RESULT"
+  exit 0
+else
+  echo "BUILD RESULT: $RESULT - Build is unsuccessful, timed out, or status could not be obtained."
+  exit 1
+fi
